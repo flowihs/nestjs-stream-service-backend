a6cf72e579f25fdf9527dd31e8c38e75c54beee4
flowihs <zmulenko500@gmail.com>
2026-02-22 23:16:38 +0300

    add session module

---
+++ src/modules/auth/session/session.resolver.ts
@@ -0,0 +1,24 @@
+import { Args, Context, Mutation, Resolver } from "@nestjs/graphql";
+
+import { SessionService } from "./session.service";
+import { UserModel } from "@/src/modules/auth/account/models/user.model";
+import type { LoginInput } from "@/src/modules/auth/session/inputs/login.input";
+import type { GqlContext } from "@/src/shared/types/gql-context.types";
+
+@Resolver("Session")
+export class SessionResolver {
+	constructor(private readonly sessionService: SessionService) {}
+
+	@Mutation(() => UserModel, { name: "login" })
+	public async login(
+		@Context() { req }: GqlContext,
+		@Args("data") input: LoginInput
+	) {
+		return this.sessionService.login(req, input);
+	}
+
+	@Mutation(() => Boolean, { name: "logout" })
+	public async logout(@Context() { req }: GqlContext) {
+		return this.sessionService.logout(req);
+	}
+}
